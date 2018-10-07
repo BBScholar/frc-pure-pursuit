a77e5bd6b9407adbed2baf2bbcf5d2911f172999
Ben Scholar <bens@digitalman.com>
2018-10-07 11:52:29 -0700

    changed calculations

--- src/org/frc2018/path/Path.java
+++ src/org/frc2018/path/Path.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-import org.frc2018.math.Point2D;
-import org.frc2018.math.Vector2;
+import org.frc2018.math.Translation2d;
 
 public class Path {
 
@@ -18,145 +17,118 @@ public class Path {
     private static final double kMaxPathAcceleration = 12.0; // in/s
 
     // waypoint list
-    private double[][] data; // [x pos, y pos, distance along path, curvature, max velocity]
+    private double[][] path;  // x y coords
+    private double[][] smooth_path; // smoothed x y coords
+    private double[] distances; // distance along path at each point
+    private double[] curvature;  // cruvature at each point
+    private double[] max_velocity; // max velocity at each point
+    private double[] target_velocity; // target velocity at each point
 
-    public Path(List<Point2D> points) {
+    public Path(List<Translation2d> points, double a) {
+        this.path = injectPoints(translationListToArray(points));
+        this.smooth_path = smoothPath(this.path, a, 1 - a, 0.001);
+        this.distances = distanceAlongPath(smooth_path);
+        this.curvature = curvatureAtPoints(smooth_path);
+        
+    }
+
+    public Path(List<Translation2d> points) {
         this(points, defaultKa);
     }
 
-    public Path(List<Point2D> points, double a) {
-        data = calculate(points, a);
-    }
 
-    private static double[][] calculate(List<Point2D> points, double a) {
-
-        double[] curvature;
-        double[] max_velocity;
-        double[] target_velocity;
-        double[] distances;
-
-        // inject points
-        points = inject(points);
-        points = smooth(points, a);
-        distances = calculateDistances(points);
-        curvature = calculateCurvature(points);
-        max_velocity = calculateMaxVelocity(curvature);
-        target_velocity = calculateTargetVelocity(max_velocity, );
-
-        return new double[1][1];
-    }
-
-    private static List<Point2D> inject(List<Point2D> points) {
-        List<Point2D> newPath = new ArrayList<>();
-        for(int i = 0; i < points.size() - 1; i++) {
-            Point2D p1 = points.get(i);
-            Point2D p2 = points.get(i + 1);
-            Vector2 v = new Vector2(p2.x - p1.x, p2.y - p1.y);
-            int num_segs = (int) Math.ceil(v.getMagnitude() / kSpacing);
-            v.normalize();
-            v.multiply(kSpacing);
-            for(int j = 0; j < num_segs; j++) {
-                Point2D np = new Point2D(p1.x + (v.x * kSpacing), p1.y + (v.y * kSpacing));
-                newPath.add(np);
+    private static double[][] injectPoints(double[][] path) {
+        List<Translation2d> newPoints = new ArrayList<>();
+        for(int i = 0; i < path.length - 1; i++) {
+            Translation2d start = new Translation2d(path[i][0], path[i][1]);
+            Translation2d end = new Translation2d(path[i+1][0], path[i+1][1]);
+            Translation2d v = new Translation2d(start, end);
+            int points_that_fit = (int) Math.ceil(v.mag() / kSpacing);
+            v = v.normalize().scale(kSpacing);
+            for (int j = 0; j < points_that_fit; j++) {
+                newPoints.add(new Translation2d( start.x() + v.x() * i, start.y() + v.y() * i));
             }
         }
-        newPath.add(points.get(points.size() - 1));
-        points = newPath;
-
-        return newPath;
+        newPoints.add(new Translation2d(path[path.length][0], path[path.length][1]));
+        return translationListToArray(newPoints);
     }
 
-    private static List<Point2D> smooth(List<Point2D> points, double a) {
-        double b = 1 - a;
-        double tolerance = 0.0001;
-        List<Point2D> newPath = new ArrayList<>();
-        for(Point2D p : points) {
-        newPath.add(p);
-        }
-    
+    private static double[][] smoothPath(double[][] path, double a, double b, double tolerance){
+        //copy array
+        double[][] newPath = doubleArrayCopy(path);
         double change = tolerance;
-        while(change >= tolerance) {
+        while(change >= tolerance){
             change = 0.0;
-            for(int i = 0; i < points.size() - 1; i++) {
-                {
-                    double aux = points.get(i).x;
-                    double pathx = aux;
-                    double newpathx = newPath.get(i).x;
-                    newPath.get(i).x += a * (pathx - newpathx) +
-                        b * (newPath.get(i - 1).x + newPath.get(i + 1).x - (2.0 * newpathx));
-                    change += Math.abs(aux - newpathx);
-                }
-                {
-                    double aux = points.get(i).y;
-                    double pathy = aux;
-                    double newpathy = newPath.get(i).y;
-                    newPath.get(i).y = a * (pathy - newpathy) +
-                         b * (newPath.get(i - 1).y + newPath.get(i + 1).y - (2.0 * newpathy));
-                    change += Math.abs(aux - newpathy);
+            for(int i = 1; i<path.length-1; i++)
+                for(int j = 0; j<path[i].length; j++){
+                    double aux = newPath[i][j];
+                    newPath[i][j] += a * (path[i][j] - newPath[i][j]) + b *(newPath[i-1][j] + newPath[i+1][j] - (2.0 * newPath[i][j]));change += Math.abs(aux - newPath[i][j]);
                 }
             }
-        }
-
         return newPath;
     }
 
-    private static double[] calculateDistances(List<Point2D> points) {
-        double[] result = new double[points.size() - 1];
-
+    private static double[] distanceAlongPath(double[][] path) {
+        double[] result = new double[path.length];
         result[0] = 0;
-        for(int i = 0; i < points.size(); i++) {
+        for (int i = 1; i < path.length; i++) {
+            Translation2d start = new Translation2d(path[i - 1][0], path[i - 1][1]);
+            Translation2d end = new Translation2d(path[i][0], path[i][1]);
+            Translation2d v = new Translation2d(start, end);
+            result[i] = result[i - 1] + v.mag();
+        }
+
+        return result;
+    }
+
+    private static double[] curvatureAtPoints(double[][] path) {
+        double[] result = new double[path.length];
+        for(int i = 1; i < path.length - 1; i++) {
+            // point we're trying to get curavture for (x1, y1)
+            Translation2d P = new Translation2d(path[i][0], path[i][1]);
+            // point behind (x2, y2)
+            Translation2d Q = new Translation2d(path[i - 1][0], path[i - 1][1]);
+            // point infront (x3, y3)
+            Translation2d R = new Translation2d(path[i + 1][0], path[i + 1][1]);
             
-        }
+            if(P.x() == Q.x()) {
+                P.setX(P.x() + 1E-10);
+            }
 
-        return result;
-    }
-
-    private static double[] calculateCurvature(List<Point2D> points) {
-        double[] result = new double[points.size()];
-
-        for(int i = 1; i < points.size() - 1; i++) {
-            double x1 = points.get(i)    .x;
-            double y1 = points.get(i)    .y;
-            double x2 = points.get(i - 1).x;
-            double y2 = points.get(i - 1).y;
-            double x3 = points.get(i + 1).x;
-            double y3 = points.get(i + 1).y;
-            if(x1 == y1) x1 += 1E-10;
-            double k1 = 0.5 * (x1 * x1 + y1 * y1 - x2 * x2 - y2 * y2) / (x1 - x2);
-            double k2 = (y1 - y1) / (x1 - x2);
-            double b = 0.5 *  Math.pow(x2, 2) + 2.0 * x2 * k1 + Math.pow(y2, 2) - Math.pow(x3, 2) + 2.0 * x3 * k1 - Math.pow(y3, 2) / (x3 * k2 - y3 + y2 - x2 * k2);
+            double k1 = 0.5 * (Math.pow(P.x(), 2) + Math.pow(P.y(), 2) - Math.pow(Q.x(), 2) - Math.pow(Q.y(), 2)) / (P.x() - Q.x());
+            double k2 = (P.y() - Q.y()) / (P.x() - Q.x());
+            double b = 0.5 * (Math.pow(P.x(),2) - 2.0 * Q.x() * k1 + Math.pow(Q.y(), 2) - Math.pow(R.x(), 2) + 2.0 * R.x() * k1 - Math.pow(R.y(), 2)) 
+                / (R.x() * k2 - R.y() + Q.y() - Q.x() * k2);
             double a = k1 - k2 * b;
-            double r = Math.sqrt(Math.pow(x1 - a, 2) + Math.pow(y1 - b, 2));
-            result[i] = 1 / r;
+            double r = Math.hypot((P.x() - a), (P.y() - b));
+            result[i] = 1.0 / r;
+        }
+        return result;
+    }
+
+    private static double[][] doubleArrayCopy(double[][] array) {
+        double[][] result = new double[array.length][array[0].length];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                result[i][j] = array[i][j];
+            }
         }
 
         return result;
     }
 
-    private static double[] calculateMaxVelocity(double[] curvature) {
-        double[] result = new double[curvature.length];
-        for (int i = 0; i < curvature.length; i++) {
-            result[i] = Math.min(kMaxPathVelocity,  2.0 / curvature[i]);
+
+
+    private static double[][] translationListToArray(List<Translation2d> points) {
+        double[][] result = new double[points.size()][2];
+        int i = 0;
+        for(Translation2d p : points) {
+            result[i][0] = p.x();
+            result[i][1] = p.y();
         }
         return result;
     }
-
-    private static double[] calculateTargetVelocity(double[] max_velocity, double[] distances) {
-        double[] result = new double[max_velocity.length];
-        result[result.length] = 0;
-        for (int i = 0; i < max_velocity.length - 1; i++) {
-            double distance = distances[i + 1] - distances[i];
-            double value = Math.sqrt(Math.pow(max_velocity[i], 2) + 2.0 * kMaxPathAcceleration * distance);
-            result[i] = Math.min(max_velocity[i], value);
-        }
-        return result;
-    }
-
-    private static double distance_formula(Point2D a, Point2D b) {
-        Vector2 v = new Vector2(a.x - b.x, a.y - b.y);
-        return v.getMagnitude();
-    }
-
 
 
 }

24c3ffa7fd700061a6843919ead3b56c0be0261c
andycate <and164v@gmail.com>
2018-10-16 13:20:28 -0700

    Updated odometry, needs to be tested

--- src/main/java/org/frc2018/Position.java
+++ src/main/java/org/frc2018/Position.java
@@ -5,6 +5,7 @@ import org.frc2018.math.Vector2;
 public class Position {
 
     private static Position _instance = new Position();
+    private static double EPSILON = 0.0001;
 
     public static Position getInstance() {
         return _instance;
@@ -25,13 +26,19 @@ public class Position {
 
     public void update(double left_value, double right_value, double angle) {
         angle = Math.toRadians(angle);
+        double angle_delta = angle - last_angle;
+        if(angle_delta == 0) angle_delta = Position.EPSILON;
         double left_delta = left_value - last_left;
         double right_delta = right_value - last_right;
         double distance = (left_delta + right_delta) / 2.0;
-        x += distance * Math.cos(angle);
-        y +=  distance * Math.sin(angle);
+        double radius_of_curvature = distance / angle_delta;
+        double delta_y = radius_of_curvature * Math.sin(angle_delta);
+        double delta_x = radius_of_curvature * (Math.cos(angle_delta) - 1);
+        x += delta_x * Math.cos(last_angle) - delta_y * Math.sin(last_angle);
+        y +=  delta_x * Math.sin(last_angle) + delta_y * Math.cos(last_angle);
         last_left = left_value;
         last_right = right_value;
+        last_angle = angle;
     }
 
     public void setPosition(Vector2 pos) {

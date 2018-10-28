fbef6bb7e987a5e06ae6205d81e637952c61d04b
andycate <and164v@gmail.com>
2018-10-28 11:48:19 -0700

    Added tests

--- src/test/java/tests/Drivetrain.java
+++ src/test/java/tests/Drivetrain.java
@@ -1,74 +1,103 @@
-// package tests;
+package tests;
 
-// import org.frc2018.Vector2;
-// import org.frc2018.Constants;
+import org.frc2018.Vector2;
+import org.frc2018.Constants;
 
-// public class Drivetrain {
-//     private static Drivetrain _instance = new Drivetrain();
+public class Drivetrain {
+    private static Drivetrain _instance = new Drivetrain();
+    public static Drivetrain getInstance() {
+        return _instance;
+    }
 
-//     public static Drivetrain getInstance() {
-//         return _instance;
-//     }
+    private double m_left_velocity, m_right_velocity;
+    private double m_left_distance, m_right_distance;
+    private double m_angle;
 
-//     private Drivetrain() {
+    private Drivetrain() {
+        m_left_velocity = 0;
+        m_right_velocity = 0;
+        m_left_distance = 0;
+        m_right_distance = 0;
+        m_angle = 0;
+    }
 
-//     }
+    public void update(double time_interval) {
+        double old_left_distance = m_left_distance;
+        double old_right_distance = m_right_distance;
+        m_left_distance += m_left_velocity * time_interval;
+        m_right_distance += m_right_velocity * time_interval;
+        double left_delta = m_left_distance - old_left_distance;
+        double right_delta = m_right_distance - old_right_distance;
+        m_angle += Math.toDegrees((right_delta - left_delta) / Constants.TRACK_WIDTH);
+        Position.getInstance().update(m_left_distance, m_right_distance, m_angle);
+    }
 
+    public void setVelocity(double left, double right) {
+        m_left_velocity = left;
+        m_right_velocity = right;
+    }
 
-// }
+    public double getRobotAngle() {
+        return m_angle;
+    }
 
-// class Position {
+    public Vector2 getRobotPosition() {
+        return Position.getInstance().getPositionVector();
+    }
+}
 
-//     private double x, y;
-//     private double last_left, last_right;
-//     private double last_angle;
+class Position {
 
-//     private Position() {
-//         x = 0;
-//         y = 0;
+    private double x, y;
+    private double last_left, last_right;
+    private double last_angle;
+
+    private Position() {
+        x = 0;
+        y = 0;
         
-//         last_left = 0;
-//         last_right = 0;
-//         last_angle = 0;
-//     }
+        last_left = 0;
+        last_right = 0;
+        last_angle = 0;
+    }
 
-//     public void update(double left_distance, double right_distance, double angle) {
-//         angle = Math.toRadians(angle);
-//         double angle_delta = angle - last_angle;
-//         if(angle_delta == 0) angle_delta = Constants.EPSILON;
-//         double left_delta = left_distance - last_left;
-//         double right_delta = right_distance - last_right;
-//         double distance = (left_delta + right_delta) / 2.0;
-//         double radius_of_curvature = distance / angle_delta;
-//         double delta_y = radius_of_curvature * Math.sin(angle_delta);
-//         double delta_x = radius_of_curvature * (Math.cos(angle_delta) - 1);
-//         y -= delta_x * Math.cos(last_angle) - delta_y * Math.sin(last_angle);
-//         x +=  delta_x * Math.sin(last_angle) + delta_y * Math.cos(last_angle);
-//         last_left = left_distance;
-//         last_right = right_distance;
-//         last_angle = angle;
-//     }
+    public void update(double left_distance, double right_distance, double angle) {
+        angle = Math.toRadians(angle);
+        double angle_delta = angle - last_angle;
+        if(angle_delta == 0) angle_delta = Constants.EPSILON;
+        double left_delta = left_distance - last_left;
+        double right_delta = right_distance - last_right;
+        double distance = (left_delta + right_delta) / 2.0;
+        double radius_of_curvature = distance / angle_delta;
+        double delta_y = radius_of_curvature * Math.sin(angle_delta);
+        double delta_x = radius_of_curvature * (Math.cos(angle_delta) - 1);
+        y -= delta_x * Math.cos(last_angle) - delta_y * Math.sin(last_angle);
+        x +=  delta_x * Math.sin(last_angle) + delta_y * Math.cos(last_angle);
+        last_left = left_distance;
+        last_right = right_distance;
+        last_angle = angle;
+    }
 
-//     public Vector2 getPositionVector() {
-//         return new Vector2(x, y);
-//     }
+    public Vector2 getPositionVector() {
+        return new Vector2(x, y);
+    }
 
-//     public void reset() {
-//         x = 0;
-//         y = 0;
-//         last_left = 0;
-//         last_right = 0;
-//         last_angle = 0;
-//     }
+    public void reset() {
+        x = 0;
+        y = 0;
+        last_left = 0;
+        last_right = 0;
+        last_angle = 0;
+    }
 
-//     @Override
-//     public String toString() {
-//         return String.format("Robot Position: X: %.2f, Y:%.2f ", x, y);
-//     }
+    @Override
+    public String toString() {
+        return String.format("Robot Position: X: %.2f, Y:%.2f ", x, y);
+    }
 
-//     private static Position _instance = new Position();
+    private static Position _instance = new Position();
 
-//     public static Position getInstance() {
-//         return _instance;
-//     }
-// }
+    public static Position getInstance() {
+        return _instance;
+    }
+}
--- src/test/java/tests/TwoCubeTest.java
+++ src/test/java/tests/TwoCubeTest.java
@@ -5,12 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.frc2018.auto.actions.DrivePathAction;
 import org.frc2018.controllers.AutoController;
+import org.frc2018.path.Path;
+import org.frc2018.path.PathFollower;
+
+import tests.Drivetrain;
 
 public class TwoCubeTest {
 
     @Test
     public void test_test() {
-        // DrivePathAction action = new DrivePathAction(path, 20);
+        Path test_path = new Path("src/test/java/tests/path.csv", false);
+        PathFollower test_follower = new PathFollower(test_path);
+        double[] velocities = test_follower.update(Drivetrain.getInstance().getRobotPosition(), Drivetrain.getInstance().getRobotAngle());
+        Drivetrain.getInstance().setVelocity(velocities[0], velocities[1]);
         assertEquals(true && false, false);
     }
 
